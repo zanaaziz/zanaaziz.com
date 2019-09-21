ac4e65448453fba10959415cf98ebb675f7dd5e6
Zana Daniel <zanadaniel@pm.me>
2019-09-21 10:01:20 +0100

    home page

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserverService } from 'src/app/shared/breakpoint-observer.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(private breakPointObserverService: BreakpointObserverService) { }
 
-  ngOnInit() {
-  }
+    isMobile: boolean;
+
+    ngOnInit() {
+        this.breakPointObserverService.isMobile()
+		.subscribe(
+			result => {
+				this.isMobile = result.matches;
+		});
+    }
 
 }
