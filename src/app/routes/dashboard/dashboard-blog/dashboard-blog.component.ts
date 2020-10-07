6850fbd6f490037246f5f1fb195f4f1cfff655d3
Zana Aziz <mail@zanaaziz.com>
2020-10-07 16:05:36 +0100

    v2.0.0-10

--- src/app/routes/dashboard/dashboard-blog/dashboard-blog.component.ts
+++ src/app/routes/dashboard/dashboard-blog/dashboard-blog.component.ts
@@ -42,7 +42,7 @@ export class DashboardBlogComponent implements OnInit {
             result => {
                 if (result === true) {
                     this.loading = true;
-                    this.api.delete(post['id'])
+                    this.api.postDelete(post.id)
                     .subscribe(
                         res => {
                             this.loading = false;
@@ -81,7 +81,7 @@ export class DashboardBlogComponent implements OnInit {
             }
         );
 
-        this.posts = new MatTableDataSource(this.api.posts());
+        this.posts = new MatTableDataSource(this.api.postsList());
         this.render();
     }
 
